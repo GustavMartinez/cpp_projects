12046772aac6afa8bcba906731e49616a7b1a06c
gust_dev <gustavo.a.t.m.12345@hotmail.com>
2026-08-19 13:59:43 -0300

    increment and decrement operators

---
+++ 06_inc_dec_operators.cpp
@@ -0,0 +1,19 @@
+// INCREMENT / DECREMENT OPERATORS
+
+/*
+++x pre-increment operator
+x++ post-increment operator
+--x pre-increment operator
+x-- post-increment operator
+
+*/
+
+#include <iostream>
+
+int main(){
+    int x = 120;
+
+    x++;
+
+    std::cout << "The value of x is: " << x << "\n";
+}
