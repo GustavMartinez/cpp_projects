e6379adff41e5fb9733ffd3ab0ff38c39462c7db
gust_dev <gustavo.a.t.m.12345@hotmail.com>
2026-08-21 07:49:40 -0300

    hacer input

---
+++ 07_input.cpp
@@ -0,0 +1,14 @@
+// Accepting one integer number and printing it
+
+#include <iostream>
+
+int main(){
+
+    std::cout << "Please enter a number and press enter: ";
+
+    int x = 0;
+
+    std::cin >> x; // meaning: read from a standard input into a x variable
+
+    std::cout << "You entered: " << x << "\n";
+}
