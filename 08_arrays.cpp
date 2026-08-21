65b912fac2a193134c55fd7cb1b5c6dd9ff8daea
gust_dev <gustavo.a.t.m.12345@hotmail.com>
2026-08-21 11:23:15 -0300

    arrays

---
+++ 08_arrays.cpp
@@ -0,0 +1,22 @@
+// Arrays
+
+// This example declares an array of 5 characters:
+
+// int main(){
+//     char arr[5];
+// }
+
+// This example declares an array of 5 ints:
+
+// int main(){
+//     int arr[5];
+// }
+
+// // To initialize an array, we can use the initialization list:
+
+#include <iostream>
+
+int main(){
+    int arr[5] = {10, 20, 30, 40, 50};
+    std::cout << "The array: " << arr[1] << '\n';
+}
